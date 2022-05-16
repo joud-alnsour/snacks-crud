022db3a5a771da946fbd534ca092d93a79139c84
joud-alnsour <alnsour.joud@gmail.com>
2022-05-16 20:38:13 +0300

    edit

--- snacks_crud_project/snacks/urls.py
+++ snacks_crud_project/snacks/urls.py
@@ -3,8 +3,8 @@ from .views import HomeView, SnackDeleteView, SnackDeleteView, SnackDetailView, 
 
 urlpatterns = [
   path('', HomeView.as_view(), name='home'),
-  path('', SnackListView.as_view(), name='snack_list'),
-  path('<int:pk>/', SnackDetailView.as_view(), name='snack_detail'),
+  path('snack-list', SnackListView.as_view(), name='snack_list'),
+  path('<int:pk>', SnackDetailView.as_view(), name='snack_detail'),
   path('create/', SnackDeleteView.as_view(), name='snack_create'),
   path('<int:pk>/update/', SnackUpdateView.as_view(), name='snack_update'),
   path('<int:pk>/delete/', SnackDeleteView.as_view(), name='snack_delete'),
